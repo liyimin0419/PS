101e0b7cdca6c28f184b10f652cdedfe7f431025
JING <1031454@,com>
2016-11-17 21:54:26 +0800

    添加两个视图

--- PSS/Areas/入库管理/Controllers/RKGLController.cs
+++ PSS/Areas/入库管理/Controllers/RKGLController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PSS.Areas.入库管理.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,14 @@ namespace PSS.Areas.入库管理.Controllers
 {
     public class RKGLController : Controller
     {
+        private PSSEntities db = new PSSEntities();
         // GET: 入库管理/RKGL
         public ActionResult RKAdd()
         {
+            return View();
+        }
+        public ActionResult NewID() {
+            var id = db.v_InDepot.ToList().OrderByDescending(x => x.InNo).First();
             return View();
         }
     }
